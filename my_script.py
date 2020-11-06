0dec8dc59286865be51fd4d8827255baceeb8307
s0240917 <srikanthreddi.a@gmail.com>
2020-11-06 19:40:21 +0530

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -1,17 +1,64 @@
-import boto3
-ec2 = boto3.resource('ec2')
+import boto.ec2
+import sys
 
-def lambda_handler(event, context):
-    filter = [
-        {
-            'Name': 'tag:Type',
-            'Values':['Scheduled']
-            
-        }
-    ]
-    instances = ec2.instances.filter(Filters=filter)
-    for instance in instances:
-        instance.stop()
-    
-    
-    return 'Success'
+# specify AWS keys
+auth = {"aws_access_key_id": "${{ secrets.AWS_ACCESS_KEY_ID }}", "aws_secret_access_key": "${{ secrets.AWS_SECRET_ACCESS_KEY }}"}
+
+def main():
+    # read arguments from the command line and 
+    # check whether at least two elements were entered
+    if len(sys.argv) < 2:
+	print "Usage: python aws.py {start|stop}\n"
+	sys.exit(0)
+    else:
+	action = sys.argv[1] 
+
+    if action == "start":
+	startInstance()
+    elif action == "stop":
+    	stopInstance()
+    else:
+    	print "Usage: python aws.py {start|stop}\n"
+
+def startInstance():
+    print "Starting the instance..."
+
+    # change "eu-west-1" region if different
+    try:
+        ec2 = boto.ec2.connect_to_region("ap-south-1", **auth)
+
+    except Exception, e1:
+        error1 = "Error1: %s" % str(e1)
+        print(error1)
+        sys.exit(0)
+
+    # change instance ID appropriately  
+    try:
+         ec2.start_instances(instance_ids="i-01e72c7d580057013")
+
+    except Exception, e2:
+        error2 = "Error2: %s" % str(e2)
+        print(error2)
+        sys.exit(0)
+
+def stopInstance():
+    print "Stopping the instance..."
+
+    try:
+        ec2 = boto.ec2.connect_to_region("ap-south-1", **auth)
+
+    except Exception, e1:
+        error1 = "Error1: %s" % str(e1)
+        print(error1)
+        sys.exit(0)
+
+    try:
+         ec2.stop_instances(instance_ids="i-01e72c7d580057013")
+
+    except Exception, e2:
+        error2 = "Error2: %s" % str(e2)
+        print(error2)
+        sys.exit(0)
+
+if __name__ == '__main__':
+    main()
